e95f00d5655f900ff6252e92a872a297141f952d
shubhamdhopate <63916048+shubhamdhopate@users.noreply.github.com>
2022-05-17 18:37:29 +0530

    Create terraform.groovy

---
+++ src/org/opstree/terraform.groovy
@@ -0,0 +1,71 @@
+package org.opstree
+def call() {
+
+inializeParams(config.codepath)
+terrafromInit()
+terraformValidate(config.codepath)
+terraformPlan(config.codepath)
+terraformAction(config.codepath)
+ 
+slackNotification("${config.slack_channel}")
+}
+
+
+def inializeParams() {
+ 
+        stage("Initializing The Project Parameters and Properties") {
+            checkout scm
+            config = readProperties file: 'Configuration'
+            echo "**************** Contents of Configuration ****************"
+            echo "${config}"
+       
+   }
+}
+
+
+
+ def terrafromInit(codePath){
+      stage("terraform init") 
+       { 
+          dir("${codePath}") {
+       
+            sh 'terraform init init'
+       }
+ }
+ }
+def terraformValidate(codePath){
+      stage("terraform validate") 
+      {        dir("${codePath}") {
+            sh 'terraform  validate'
+      }
+ }
+ }
+def terraformPlan(codePath){
+        stage("terraform plan") 
+      {   dir("${codePath}") {
+          sh 'terraform  plan '
+         
+          }
+
+  }
+  
+  }
+
+  
+def terraformAction(codePath){
+          stage("terraform apply") 
+        {    dir("${codePath}") {
+          
+                   sh 'terraform apply  -auto-approve'
+                }
+
+          
+        }
+       
+        }
+
+ def slackNotification(channel){
+    stage("slack notification") {   
+ slackSend(channel:"${channel}" , message:"job is successful '${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|>)'")
+    }
+}
